a9f07f0a1da6b056baa591fe85b758fb00890af4
Sunny Yang <z5487903@ad.unsw.edu.au>
2023-09-19 19:33:43 +1000

    Drafted object structures for kahoot ripoff

---
+++ src/quiz.js
@@ -0,0 +1,15 @@
+function adminQuizList ( authUserId ) {
+    return { quizzes: [
+        {
+          quizId: 1,
+          name: 'My Quiz',
+        }
+      ]
+    }
+}
+
+function adminQuizCreate ( authUserId, name, description ) {
+    return {
+        quizId: 2
+      }
+}
