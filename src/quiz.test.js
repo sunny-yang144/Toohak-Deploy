c0335da78fb739db1cdac814d8753926507ae3c9
Shannon <z5470446@ad.unsw.edu.au>
2023-09-29 14:46:12 +1000

    Generalised email and password

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -69,7 +69,7 @@ describe('Tests for adminQuizNameUpdate', () => {
   // Clear and create a valid quiz and user for the test to apply adminQuizNameUpdate
   beforeEach(() => {
     clear();
-    const user = adminAuthRegister('hehexd@gmail.com', 'ilovetyler1', 'Tyler', 'One');
+    const user = adminAuthRegister('valid@gmail.com', 'validPassword1', 'Tyler', 'One');
     const quiz = adminQuizCreate(user.authUserId, 'PC games quiz', 'FPS games only');
   });
 
