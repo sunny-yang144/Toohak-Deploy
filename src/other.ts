a8a871c3b149bd6995bd8475d06b04d07d412c5c
Sunny Yang <z5487903@ad.unsw.edu.au>
2023-10-17 20:02:35 +1100

    Documented generateId and generateToken to make future string encoding easier

--- src/other.ts
+++ src/other.ts
@@ -8,6 +8,14 @@ export function clear (): Record<string, never> {
   return {}
 }
 
+/**
+ * @param { Token[] } idArray
+ * 
+ * Takes in an array, finds the highest SessionId then returns a number higher 
+ * than all current sessionId in the dataStore
+ * 
+ * @returns { Number } Max + 1
+ */
 function generateId (idArray: Token[]) {
   if (idArray.length === 0) {
     return 0;
@@ -21,6 +29,15 @@ function generateId (idArray: Token[]) {
   return max + 1;
 }
 
+/**
+ * 
+ * @param { DataStore } data 
+ * @param { User } user 
+ * Generates a sessionId, makes a new token and pushes it to the respective user,
+ * then pushes to a tokens array with a reference to the user who owns the token.
+ * 
+ * @returns { Number } Token
+ */
 export function generateToken (data: DataStore, user: User) {
   const sessionId = generateId(data.tokens);
   const token = {
